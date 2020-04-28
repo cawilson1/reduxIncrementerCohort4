5f4fdfa2b69acccdeb1908580bd8bd861c3d31aa
cawilson1 <cawilson1@g.cofc.edu>
2020-04-28 11:17:36 -0400

    make simple createstore with automatic state completion in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,30 @@
-export const reducer = (state, action) => {
+export const reducer = (
+  state = {
+    counter: 0,
+    error: null
+  },
+  action
+) => {
   switch (action.type) {
     case "INCREMENT_COUNTER": {
-      return { ...state, counter: state.counter + 1, error: null };
+      return { ...state, counter: state.counter + 1 };
     }
     case "DECREMENT_COUNTER": {
-      return { ...state, counter: state.counter - 1, error: null };
+      return { ...state, counter: state.counter - 1 };
     }
     case "INCREMENT_BY_N": {
       return {
         ...state,
-        counter: state.counter + Number(action.n),
-        error: null
+        counter: state.counter + Number(action.n)
       };
     }
     case "DECREMENT_BY_N": {
       return {
         ...state,
-        counter: state.counter - Number(action.n),
-        error: null
+        counter: state.counter - Number(action.n)
       };
     }
-    default: {
-      return { ...state, error: true };
-    }
+    default:
+      return { ...state };
   }
 };
--- src/store/CreateStore.js
+++ src/store/CreateStore.js
@@ -1,11 +1,9 @@
 import { useReducer } from "react";
 
-const initialState = {
-  counter: 0,
-  error: null
-};
-
 export function CreateStore(reducerFn) {
-  const [state, dispatch] = useReducer(reducerFn, initialState);
+  const [state, dispatch] = useReducer(
+    reducerFn,
+    reducerFn(undefined, "") //console log this line somewhere if there is confusion
+  );
   return { state, dispatch }; //this object is the store; it contains the current state and the dispatch to mutate it
 }
