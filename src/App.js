5919737fbd8aa2715041468282b409ddd11c2e6a
cawilson1 <cawilson1@g.cofc.edu>
2020-04-28 10:06:37 -0400

    create counter with useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const reducer = (state, action) => {
+  if (action.type === "INCREMENT_COUNTER") {
+    return { ...state, counter: state.counter + 1, error: null };
+  }
+  if (action.type === "DECREMENT_COUNTER") {
+    return { ...state, counter: state.counter - 1, error: null };
+  }
+  if (action.type === "SET_ERROR") {
+    return { ...state, error: true };
+  }
+};
+
+const initialState = {
+  counter: 0,
+  error: null
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {state.counter}
+        <button
+          onClick={e => {
+            e.preventDefault();
+            dispatch({ type: "INCREMENT_COUNTER" });
+          }}
         >
-          Learn React
-        </a>
+          Increment
+        </button>
+        <button
+          onClick={e => {
+            e.preventDefault();
+            dispatch({ type: "DECREMENT_COUNTER" });
+          }}
+        >
+          Decrement
+        </button>
       </header>
     </div>
   );
