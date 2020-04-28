d897eba968a38b32e1dd7997a752506473d73f27
cawilson1 <cawilson1@g.cofc.edu>
2020-04-28 10:34:13 -0400

    move reducers to separate file

--- src/App.js
+++ src/App.js
@@ -6,34 +6,7 @@ import {
   incrementByN,
   decrementByN
 } from "./actions";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "INCREMENT_COUNTER": {
-      return { ...state, counter: state.counter + 1, error: null };
-    }
-    case "DECREMENT_COUNTER": {
-      return { ...state, counter: state.counter - 1, error: null };
-    }
-    case "INCREMENT_BY_N": {
-      return {
-        ...state,
-        counter: state.counter + Number(action.n),
-        error: null
-      };
-    }
-    case "DECREMENT_BY_N": {
-      return {
-        ...state,
-        counter: state.counter - Number(action.n),
-        error: null
-      };
-    }
-    default: {
-      return { ...state, error: true };
-    }
-  }
-};
+import { reducer } from "./reducers";
 
 const initialState = {
   counter: 0,
